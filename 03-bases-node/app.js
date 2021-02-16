8d7088d67b02d3bb7b671470f18c443ff1601541
Jonathan Meriño <jmerino1204@gmail.com>
2021-02-16 07:21:45 -0500

    yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,7 +1,18 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 console.clear();
 
+console.log(process.argv);
+console.log(argv);
 
-let numerotabla = 3;
+console.log('base yargs', argv.base);
 
-crearArchivo(numerotabla).then(msg => console.log(msg)).catch(err => console.log(err));
+
+/*const [, , arg3 = 'base=5'] = process.argv
+const [, base = 5] = arg3.split('=');
+console.log(base);
+let numerotabla = 3;*/
+
+/*crearArchivo(base)
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));*/
