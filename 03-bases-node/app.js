353f2ad749be19ae8fed5bf480b50580601938d6
Jonathan Meriño <jmerino1204@gmail.com>
2021-02-16 07:46:41 -0500

    yargs base si quiere listar o no

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,28 @@
+const { option, boolean } = require('yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs').argv;
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true
+    }).check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La  base tiene que se un numero';
+        } else {
+            return true;
+        }
+    }).option('l', {
+        alias: 'listar',
+        type: boolean,
+        default: false,
+        describe: 'Comando para listar'
+    }).argv;
+
 console.clear();
 
-console.log(process.argv);
-console.log(argv);
-
 console.log('base yargs', argv.base);
+console.log('base yargs', argv);
+
 
 
 /*const [, , arg3 = 'base=5'] = process.argv
@@ -13,6 +30,6 @@ const [, base = 5] = arg3.split('=');
 console.log(base);
 let numerotabla = 3;*/
 
-/*crearArchivo(base)
+crearArchivo(argv.b, argv.l)
     .then(msg => console.log(msg))
-    .catch(err => console.log(err));*/
+    .catch(err => console.log(err));
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
-const crearArchivo = async (base = 5) => {
+const crearArchivo = async (base = 5, listar) => {
     try {
-        console.log("==================");
-        console.log("TABLA DEL:", base);
-        console.log("==================");
-
         let salida = ''
         for (let i = 1; i <= 10; i++) {
             salida += (`${base} x ${i} = ${base * i}\n`);
         }
-        console.log(salida);
+        if (listar) {
+            console.log("==================");
+            console.log("TABLA DEL:", base);
+            console.log("==================");
+            console.log(salida);
+        }
 
         fs.writeFileSync(`tabla-${base}.txt`, salida);
         return `Creado la tabla${base}-creada`;
