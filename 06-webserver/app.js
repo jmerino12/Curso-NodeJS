ae096731cd3614fef5f783912dc887288ce9a0b4
Jonathan Meriño <jmerino1204@gmail.com>
2021-03-02 05:41:58 -0500

    sitio web completo

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -5,9 +5,14 @@ const app = express()
 app.use(express.static('public'))
 
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html')
 })
+
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html')
+})
+
 
 app.get('/hola-mundo', function (req, res) {
     res.send('hola mundo en su respectiva ruta')
