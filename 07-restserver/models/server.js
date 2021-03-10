f9e13d86a404db0ea43e1f091b946b693cd316f0
Jonathan Meriño <jmerino1204@gmail.com>
2021-03-10 06:08:04 -0500

    model mongoosedb

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -1,29 +1,36 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { dbConnection } = require('../DB/config');
 
-class Server{
-    constructor(){
+class Server {
+    constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usuariosPath = '/api/usuarios'
+        this.usuariosPath = '/api/usuarios';
+        //Conectar a BD
+        this.conectarDB();
         //Middlewares
         this.middlwares();
         this.routes();
     }
 
-    middlwares(){
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    middlwares() {
         this.app.use(cors());
         this.app.use(express.json())           //Lecutra y paseo del body
         this.app.use(express.static('public'));
     }
 
-    routes(){
-        this.app.use(this.usuariosPath,require('../routes/usuarios.routes'));
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/usuarios.routes'));
     }
 
-    lister(){
-        this.app.listen(this.port, () =>{
-            console.log('Sevidor corriendo en el puerto',this.port)
+    lister() {
+        this.app.listen(this.port, () => {
+            console.log('Sevidor corriendo en el puerto', this.port)
         })
     }
 }
