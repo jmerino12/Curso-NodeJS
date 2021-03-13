83a319128de77de891f8544dfccfb9777283d691
Jonathan Meriño <jmerino1204@gmail.com>
2021-03-13 17:21:53 -0500

    Paginador

--- 07-restserver/controllers/usuarios.controller.js
+++ 07-restserver/controllers/usuarios.controller.js
@@ -6,11 +6,21 @@ const Usuario = require('../models/usuario');
 const usuariosGET = async (req = request, res = response) => {
     //const { q, nombre = "Non", apikey, page = 1, limit } = req.query
     const { limite = 5, desde = 0 } = req.query;
-    const usuarios = await Usuario.find()
-        .skip(Number(desde))
-        .limit(Number(limite));
+    const query = { estado: true };
+    /* const usuarios = await Usuario.find(query)
+         .skip(Number(desde))
+         .limit(Number(limite));*/
+
+    const [total, Usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+
+    ])
     res.json({
-        usuarios
+        total,
+        Usuarios
     });
 }
 
