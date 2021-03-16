367cc6ad8ade101ca1e43fa9c2daa42cd83ab7d4
Jonathan Meriño <jmerino1204@gmail.com>
2021-03-16 07:25:23 -0500

    Modelo categoria

---
+++ 07-restserver/models/categoria.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const CategoriaSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatoria']
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usario',
+        required: [true, 'El usuario de creacion es obligatorio']
+    }
+});
+
+module.exports = model('Categoria', CategoriaSchema);
