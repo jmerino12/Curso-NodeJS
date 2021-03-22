c1628fbafa758e01bd5c89f1dabba73f33030419
Jonathan Meriño <jmerino1204@gmail.com>
2021-03-22 10:17:28 -0500

    Modelo chat-mensajes

--- 10-socket-chat/models/index.js
+++ 10-socket-chat/models/index.js
@@ -1,6 +1,5 @@
-
-
 const Categoria = require('./categoria');
+const ChatMensajes = require('./chat-mensajes');
 const Producto = require('./producto');
 const Role = require('./role');
 const Server = require('./server');
@@ -14,5 +13,6 @@ module.exports = {
     Role,
     Server,
     Usuario,
+    ChatMensajes
 }
 
